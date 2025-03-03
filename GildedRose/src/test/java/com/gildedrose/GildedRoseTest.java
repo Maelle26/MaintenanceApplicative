8f0008c62a836b21c19ed210ab15cce03adb3816
Maelle26 <maelle.bitsindou2@etu.univ-lorraine.fr>
2025-03-03 09:34:42 +0100

    92% code coverage sans modif code

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,114 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+//    //Test qui compare les noms des 2 articles
+//    @Test
+//    void foo() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals("fixme", app.items[0].name);
+//    }
+
+    //Test qui vérifie que la qualité ne soit jamais négative
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void updateStandardItemQuality(){
+        Item[] items = new Item[] {new Item ("carotte", 2, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
     }
+
+    //Test quand un produit standard à un sellin < 0 et que la qualité est supérieure à 0
+    @Test
+    public void updateStandardItemSellIn(){
+        Item[] items = new Item[] {new Item ("carotte", -1, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+    }
+
+    //Test que la qualité ne soit jamais supérieure à 50 (produits lambda)
+    @Test
+    public void updateStandardItemMaxQuality(){
+        Item[] items = new Item[] {new Item ("carotte", 2, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(49, app.items[0].quality);
+
+    }
+
+    //Aged Brie
+
+    //Test que la qualité du "Aged Brie" augmente
+    @Test
+    public void updateAgedBrieQuality(){
+        Item[] items = new Item[] {new Item ("Aged Brie", 2, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].quality);
+    }
+
+    //Test que la qualité di "Aged Brie" ne dépasse pas 50
+    @Test
+    public void updateAgedBrieMaxQuality(){
+        Item[] items = new Item[] {new Item ("Aged Brie", 2, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
+    //Sulfuras
+
+    //Test que la qualité de "Sulfuras" soit toujours à 80
+    @Test
+    public void updateAgedSulfurasQuality(){
+        Item[] items = new Item[] {new Item ("Sulfuras, Hand of Ragnaros", 2, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+    }
+
+    //Backstage passes
+
+    //Test  La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la
+    // qualité tombe à 0 après le concert.
+    @Test
+    public void updateAgedBackstagePassesQuality(){
+        Item[] items = new Item[] {new Item ("Backstage passes to a TAFKAL80ETC concert", 10, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(22, app.items[0].quality);
+    }
+
+    @Test
+    public void updateAgedBackstagePassesQuality2(){
+        Item[] items = new Item[] {new Item ("Backstage passes to a TAFKAL80ETC concert", 5, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(23, app.items[0].quality);
+    }
+
+    //Teste quand il rne rste plus de jours
+    @Test
+    public void updateAgedBackstagePassesQuality3(){
+        Item[] items = new Item[] {new Item ("Backstage passes to a TAFKAL80ETC concert", 0, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    //Conjured
+
+    //Test que la qualité de "Conjured" diminue 2 foiq plus vite que les autres produits
+//    @Test
+//    public void updateConjuredQuality(){
+//        Item[] items = new Item[] {new Item ("Conjured", 2, 20)};
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals(18, app.items[0].quality);
+//    }
+
+
 
 }
